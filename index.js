0813f410fe447d60b65f7f357312419583085d0c
LuonggNguyen <87462814+LuonggNguyen@users.noreply.github.com>
2022-03-31 23:53:20 +0700

    test git

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const  app = express();
 const port = 3000;
 
 app.get('/', (req,res) => {
-    return res.send("ASM NodeJs");
+    return res.send("ASM NodeJs PD04303");
 });
 app.listen(port, () => console.log("ASM App listening at http://localhost:"+port));
