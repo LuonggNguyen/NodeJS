1536e037d665e242e1044901fbac4e304264f455
LuonggNguyen <87462814+LuonggNguyen@users.noreply.github.com>
2022-04-02 07:37:01 +0700

    Lan2

--- index.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const  app = express();
-const port = 3000;
-
-app.get('/', (req,res) => {
-    return res.send("ASM NodeJs PD04303");
-});
-app.listen(port, () => console.log("ASM App listening at http://localhost:"+port));
