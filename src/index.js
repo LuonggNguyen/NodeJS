480bd4644c0af21abdc15200fc30524d5c7f3372
LuonggNguyen <87462814+LuonggNguyen@users.noreply.github.com>
2022-04-06 04:09:48 +0700

    Refactor code

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ const express = require("express");
 const morgan = require('morgan');
 const  app = express();
 const port = 3000;
+
+const route = require('./routes');
+
+
 app.use(express.static(path.join(__dirname,'public')));
 app.use(morgan('combined'));
 app.use(express.urlencoded({
@@ -17,10 +21,5 @@ app.engine('hbs', handlebars.engine({
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname,'resources/views'));
 
-app.get('/', (req, res) => {
-    res.render('home');
-});
-app.get('/search', (req, res) => {
-    res.render('search');
-});
+route(app);
 app.listen(port, () => console.log("ASM App listening at http://localhost:"+port));
