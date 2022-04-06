f81222c619f376258b93268ffed264f44c4d5689
LuonggNguyen <87462814+LuonggNguyen@users.noreply.github.com>
2022-04-06 08:03:58 +0700

    k

--- src/index.js
+++ src/index.js
@@ -6,6 +6,10 @@ const app = express();
 const port = 3000;
 
 const route = require('./routes');
+const db = require('./config/db');
+
+//connect DB
+db.connect();
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(morgan('combined'));
@@ -26,7 +30,7 @@ app.engine(
 );
 
 app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'resources/views'));
+app.set('views', path.join(__dirname, 'resources','views'));
 
 route(app);
 
