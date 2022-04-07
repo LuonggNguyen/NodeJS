e47efd123cc64496f504a8b8f223b94b26906c90
LuonggNguyen <87462814+LuonggNguyen@users.noreply.github.com>
2022-04-08 05:25:45 +0700

    kocan

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -3,23 +3,22 @@ const Course = require('../models/Course');
 class SiteController {
     index(req, res, next) {
         Course.find({})
-        .then(courses => res.render('home'))
-        .catch(next);
+            .then((courses) => res.render('home', { courses }))
+            .catch(next);
         // Course.find({}, function(err, courses){
         //     if(!err) {
         //         res.json(courses);
         //     }else{
         //         next(err);
         //         res.status(400).json({error: 'ERROR'});
-        //     }            
+        //     }
         // });
-        
-        // res.render('home');
 
-    };
+        // res.render('home');
+    }
     search(req, res) {
         res.render('search');
-    };
+    }
 }
 
 module.exports = new SiteController();
